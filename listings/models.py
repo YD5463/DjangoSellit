533b4bd3625bb816e8b34eee10c836a2da1cf1a2
Yosef Danan <yosefdanan1234@gmail.com>
2021-03-22 08:30:13 +0200

    reorder and start listing models

---
+++ listings/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.utils import timezone
+
+
+class Category(models.Model):
+	name = models.CharField(max_length=50)
+	color = models.CharField(max_length=50)
+	icon = models.CharField(max_length=50)
+
+	def __str__(self):
+		return self.name
+
+
+class Listing(models.Model):
+	name = models.CharField(max_length=50)
+	category = models.ForeignKey(Category,on_delete=models.PROTECT)
+	price = models.IntegerField()
+	description = models.CharField(max_length=300)
+	published_time = models.DateTimeField(default=timezone.now)
+
+	def __str__(self):
+		return self.name  # todo: change this print
+
+
+class ListingImages(models.Model):
+	image = models.ImageField(upload_to="")
+	listing_id = models.ForeignKey(Listing,on_delete=models.CASCADE)
